9beabcda6928dbee27f4d195b4a6db0f5d18f6b8
ian simpson <iansimpson55@hotmail.com>
2020-06-05 11:38:15 +0100

    added controller

---
+++ net-core-api-auth/Controllers/PeopleController.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using net_core_api_auth.Models;
+
+namespace net_core_api_auth.Controllers
+{
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet("people/all")]
+        public ActionResult<IEnumerable<Person>> GetAll()
+        {
+            return new[]
+            {
+                new Person { Name = "Ana" },
+                new Person { Name = "Felipe" },
+                new Person { Name = "Emillia" }
+            };
+        }
+    }
+}
---
+++ net-core-api-auth/Models/People.cs
@@ -0,0 +1,7 @@
+namespace net_core_api_auth.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
